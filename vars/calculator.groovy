63e758c1c147a92bd7984f58c9f5aa1dbd5745b5
ammarjamil <mohdammar.jamil@gmail.com>
2021-10-06 00:51:04 +0530

    added calculator.groovy file in vars folder

---
+++ vars/calculator.groovy
@@ -0,0 +1,13 @@
+def add(a,b){
+	echo "Addition of ${a} and ${b} is ${a+b}"
+}
+def mul(a,b){
+	echo "Multiplication of ${a} and ${b} is ${a*b|}"
+}
+def div(a,b){
+	echo "Division of ${a} and ${b} is ${a/b}"
+}
+def modu(a,b){
+	echo "Remaindedr of ${a} and ${b} is ${a%b}"
+}
+
