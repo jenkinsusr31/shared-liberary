a45e845df3be36ae2aa844ff06b12e29f69754f0
ammarjamil <mohdammar.jamil@gmail.com>
2021-10-06 01:15:04 +0530

    fix calculator.groovy script

--- vars/calculator.groovy
+++ vars/calculator.groovy
@@ -2,7 +2,7 @@ def add(a,b){
 	echo "Addition of ${a} and ${b} is ${a+b}"
 }
 def mul(a,b){
-	echo "Multiplication of ${a} and ${b} is ${a*b|}"
+	echo "Multiplication of ${a} and ${b} is ${a*b}"
 }
 def div(a,b){
 	echo "Division of ${a} and ${b} is ${a/b}"
